f8789a5f00b7eb0bc920e94626cc714c198a362d
redknot <redknotmiaoyuqiao@gmail.com>
2020-01-05 03:49:22 +0800

    gl

---
+++ Lib/EyerGL/EyerGL.hpp
@@ -0,0 +1,14 @@
+#ifndef	EYER_LIB_GL_AV_H
+#define	EYER_LIB_GL_AV_H
+
+namespace Eyer
+{
+    class EyerGLWindow
+    {
+    public:
+        EyerGLWindow();
+        ~EyerGLWindow();
+    };
+}
+
+#endif
---
+++ Lib/EyerGL/EyerGLWindow.cpp
@@ -0,0 +1,18 @@
+//
+// Created by redknot on 2020/1/5.
+//
+#include "EyerGL.hpp"
+
+namespace Eyer
+{
+    EyerGLWindow::EyerGLWindow()
+    {
+
+    }
+
+    EyerGLWindow::~EyerGLWindow()
+    {
+
+    }
+}
+
