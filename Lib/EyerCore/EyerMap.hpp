05163b7bef28a507aefb7b73d32fd10b10b6f69d
YaoyaoloveQiaoqiao <632212519@qq.com>
2020-02-18 23:26:10 +0800

    map define

---
+++ Lib/EyerCore/EyerMap.hpp
@@ -0,0 +1,29 @@
+#ifndef	EYER_MAP_H
+#define	EYER_MAP_H
+
+namespace Eyer {
+
+	template<typename K, typename V>
+	class EyerMap
+	{
+	public:
+		EyerMap();
+		~EyerMap();
+
+
+	
+
+	};
+
+	template<typename K, typename V>
+	EyerMap<typename K, typename V>::EyerMap()
+	{
+	}
+    
+	template<typename K, typename V>
+	EyerMap<typename K, typename V>::~EyerMap()
+	{
+	}
+}
+
+#endif
