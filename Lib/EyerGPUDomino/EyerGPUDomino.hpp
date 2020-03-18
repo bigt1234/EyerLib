b6388d3384b6a37e3d1faa27ef9a368acc26b50b
redknot <redknotmiaoyuqiao@gmail.com>
2020-03-18 18:25:17 +0800

    zoom blur

--- Lib/EyerGPUDomino/EyerGPUDomino.hpp
+++ Lib/EyerGPUDomino/EyerGPUDomino.hpp
@@ -67,6 +67,15 @@ namespace Eyer
         virtual int Go(EyerGLTexture * input, EyerGLTexture * output, int width, int height);
     };
 
+    class EyerGominoZoomBlur: public EyerGomino
+    {
+    public:
+        EyerGominoZoomBlur();
+        ~EyerGominoZoomBlur();
+
+        virtual int Go(EyerGLTexture * input, EyerGLTexture * output, int width, int height);
+    };
+
     class EyerGaussianBlurComponent : public EyerGLComponent
     {
     public:
@@ -111,6 +120,28 @@ namespace Eyer
         int h = 1080;
     };
 
+
+
+    class EyerCommonComponent : public EyerGLComponent
+    {
+    public:
+        EyerCommonComponent(char * V_SHADER, char * F_SHADER);
+        ~EyerCommonComponent();
+
+        virtual int Draw();
+
+        int SetTexture(EyerGLTexture * _texture);
+
+        int SetWH(int _w, int _h);
+
+    private:
+        EyerGLDraw * draw = nullptr;
+        EyerGLVAO * vao = nullptr;
+        EyerGLTexture * texture = nullptr;
+
+        int w = 1920;
+        int h = 1080;
+    };
 }
 
 #endif //EYE_LIB_EYERGPUDOMINO_H
---
+++ Lib/EyerGPUDomino/EyerGominoZoomBlur.cpp
@@ -0,0 +1,84 @@
+#include "EyerGPUDomino.hpp"
+#include "EyerGL/ShaderH.hpp"
+
+namespace Eyer
+{
+    EyerGominoZoomBlur::EyerGominoZoomBlur() : EyerGomino("ZoomBlur")
+    {
+
+    }
+
+    EyerGominoZoomBlur::~EyerGominoZoomBlur()
+    {
+
+    }
+
+    int EyerGominoZoomBlur::Go(EyerGLTexture * input, EyerGLTexture * output, int width, int height)
+    {
+        char * V_SHADER = (char *)SHADER(
+            layout (location = 0) in vec3 pos;
+            layout (location = 1) in vec3 coor;
+
+            out vec3 outCoor;
+
+            void main()
+            {
+                outCoor = coor;
+                gl_Position = vec4(pos, 1.0);
+            }
+        );
+
+        char * F_SHADER = (char *)SHADER(
+            out vec4 colorFrag;
+            uniform sampler2D imageTex;
+            in vec3 outCoor;
+
+            uniform float w;
+            uniform float h;
+
+
+            float perspective = 0.3;
+
+            const int samples = 200;
+            const float minBlur = 0.1;
+            const float maxBlur = 0.3;
+            const float speed = 3.0;
+
+            void main(){
+                vec2 TexCoords = vec2(outCoor.x, outCoor.y);
+                // vec3 rgb = texture(imageTex, TexCoords).rgb;
+
+                vec2 p = TexCoords;
+
+                vec4 result = vec4(0);
+
+                float timeQ = mix(minBlur, maxBlur, 0.5);
+                for (int i=0; i<=samples; i++)
+                {        
+                    float q = float(i) / float(samples);
+                    result += texture(imageTex, p + (vec2(0.5)-p)*q*timeQ)/float(samples);
+                }
+
+                colorFrag = result;
+            }
+        );
+
+
+        EyerGLFrameBuffer frameBuffer(width, height, output);
+
+        frameBuffer.ClearAllComponent();
+        frameBuffer.Clear();
+
+        EyerCommonComponent commonComponent(V_SHADER, F_SHADER);
+        commonComponent.SetTexture(input);
+        commonComponent.SetWH(width, height);
+
+        frameBuffer.AddComponent(&commonComponent);
+
+        frameBuffer.Draw();
+
+        frameBuffer.ClearAllComponent();
+
+        return 0;
+    }
+}
