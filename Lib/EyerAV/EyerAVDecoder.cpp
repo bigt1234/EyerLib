fb2fd321be582c94c3c9ac492c4779ea8f3f4a77
Redknot <redknotmiaoyuqiao@gmail.com>
2020-04-12 11:27:10 +0800

    hw decoder

--- Lib/EyerAV/EyerAVDecoder.cpp
+++ Lib/EyerAV/EyerAVDecoder.cpp
@@ -5,6 +5,8 @@
 #include "EyerAVPacketPrivate.hpp"
 #include "EyerAVFramePrivate.hpp"
 
+
+
 namespace Eyer
 {
     EyerAVDecoder::EyerAVDecoder()
@@ -25,14 +27,57 @@ namespace Eyer
         }
     }
 
+#ifdef EYER_HW_DECODER
+    static bool IsSupportHW(AVCodec * codec, AVHWDeviceType type)
+    {
+        for (int i = 0; ; i++) {
+            const AVCodecHWConfig * config = avcodec_get_hw_config(codec, i);
+            if (!config) {
+                break;
+            }
+
+            if (config->methods & AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX && config->device_type == type) {
+                // hw_pix_fmt = config->pix_fmt; //获取硬件支持的图像格式
+                return true;
+            }
+        }
+        return false;
+    }
+#endif
+
     int EyerAVDecoder::InitHW(EyerAVStream * stream)
     {
         avcodec_parameters_to_context(piml->codecContext, stream->piml->codecpar);
         AVCodec * codec = avcodec_find_decoder(piml->codecContext->codec_id);
 
-        if(codec->id == AVCodecID::AV_CODEC_ID_H264){
+#ifdef EYER_HW_DECODER
 
+        if(codec->id == AVCodecID::AV_CODEC_ID_H264){
+            Eyer::EyerString hwList[] = {"dxva2"};
+
+            int size = sizeof(hwList) / sizeof(Eyer::EyerString);
+
+            for(int hwIndex = 0; hwIndex < size; hwIndex++){
+                enum AVHWDeviceType type = av_hwdevice_find_type_by_name(hwList[hwIndex].str);
+                if(type == AVHWDeviceType::AV_HWDEVICE_TYPE_NONE){
+                    continue;
+                }
+
+                bool support = IsSupportHW(codec, type);
+                if(!support){
+                    continue;
+                }
+
+                AVBufferRef *hw_device_ctx = NULL;
+                if ((err = av_hwdevice_ctx_create(&hw_device_ctx, type, NULL, NULL, 0)) < 0) {
+                    continue;
+                }
+
+                piml->codecContext->hw_device_ctx = hw_device_ctx;
+            }
         }
+
+#endif
 
         int ret = avcodec_open2(piml->codecContext, codec, nullptr);
         if(ret){
