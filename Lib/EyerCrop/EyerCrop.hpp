c0d385c15b941c5204031d1ff2716c3203964b3d
redknot <redknotmiaoyuqiao@gmail.com>
2020-04-29 18:21:42 +0800

    av crop

--- Lib/EyerCrop/EyerCrop.hpp
+++ Lib/EyerCrop/EyerCrop.hpp
@@ -15,7 +15,28 @@ namespace Eyer
         EyerAVCropUtil();
         ~EyerAVCropUtil();
 
-        int GetCrop(int viewW, int viewH, int imageW, int imageH, int & targetW, int & targetH,EyerAVCropType cropType = EyerAVCropType::FIT_CENTER);
+        int GetCrop(int viewW, int viewH, int imageW, int imageH, int & targetW, int & targetH, EyerAVCropType cropType = EyerAVCropType::FIT_CENTER);
+    };
+
+
+    class EyerAVCrop
+    {
+    public:
+        EyerAVCrop(int viewW, int viewH, int imageW, int imageH);
+        ~EyerAVCrop();
+
+        int GetCropW();
+        int GetCropH(); 
+
+        int Crop(EyerAVCropType cropType = EyerAVCropType::FIT_CENTER);
+    private:
+        int viewW = 0;
+        int viewH = 0;
+        int imageW = 0;
+        int imageH = 0;
+
+        int cropW = 0;
+        int cropH = 0;
     };
 }
 
